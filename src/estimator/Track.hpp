5852dfc311c9281226d3dad45af3c003b065d3d8
yanyan-li <yanyan.li.camp@gmail.com>
2022-10-01 11:55:03 +0800

    [feat](Track): estimate rigid rotation based on venom frames

--- src/estimator/Track.hpp
+++ src/estimator/Track.hpp
@@ -1,7 +1,7 @@
 /*** 
  * @Author: yanyan-li yanyan.li.camp@gmail.com
  * @Date: 2022-09-25 05:34:55
- * @LastEditTime: 2022-09-30 17:04:28
+ * @LastEditTime: 2022-10-01 03:15:24
  * @LastEditors: yanyan-li yanyan.li.camp@gmail.com
  * @Description: 
  * @FilePath: /venom/src/estimator/Track.hpp
@@ -49,6 +49,9 @@ namespace simulator
                 std::vector<std::pair<int, Eigen::Vector3d>> vec_type_direction;
                 //std::vector<Eigen::Vector3d> vec_vd;
                 std::map<int, Eigen::Vector3d> VenomFrame;
+                // 
+                std::vector<Eigen::Vector3d> venom_axis;
+                venom_axis = std::vector<Eigen::Vector3d>(3, Eigen::Vector3d::Zero());
                 std::pair<int/*venom_id*/, Eigen::Matrix3d> r_venom_frame; 
                 for (int observ_ml_i = 0, observ_ml_end = robot_traject_->obs_line_[frame_id].size(); observ_ml_i < observ_ml_end; observ_ml_i++)
                 {
@@ -75,25 +78,111 @@ namespace simulator
                 if(set_vd_type.size()>=2) // Vanom is detected
                 {
                     std::cout<<"venom is detected"<<std::endl;
-                    VenomFrameGeneration(vec_type_direction, VenomFrame); 
+                    int venom_type = VenomFrameGeneration(vec_type_direction, venom_axis); 
+                
                 }    
             }
         }
 
-        void VenomFrameGeneration(std::vector<std::pair<int, Eigen::Vector3d>> &type_detections, std::map<int, Eigen::Vector3d> &venom_frame)
+        int VenomFrameGeneration(std::vector<std::pair<int, Eigen::Vector3d>> &type_detections, std::vector<Eigen::Vector3d> &venom_axis)
         {
+            // type 0: 0, 1
+            // type 1: 0, 2
+            // type 2: 0, 1, 2
+            // type 3: 1, 2
+             
+            assert(type_detections.size()>=2);
+            int venom_type = -1;
             for (auto mit = type_detections.begin(), mit_end = type_detections.end(); mit != mit_end; mit++)
             {
                 std::cout<<"before," << mit->first << ", " << mit->second << std::endl;
-                venom_frame[mit->first] = mit->second;
+                if(mit->first==0)
+                {
+                    venom_axis[0] = mit->second;
+                    venom_type =0;
+                }
+                else if(mit->first==1)
+                {
+                    venom_axis[1] = mit->second;
+                    if (venom_type==0)
+                    {
+                        venom_type = 1;
+                    }else if(venom_type==-1)
+                    {
+                        venom_type = 3; 
+                    }
+                }
+                else if(mit->first==2)
+                    venom_axis[2] = mit->second;        
             }
+            return venom_type;
         }
 
-        // void RotationCamera2Venom(std::map<int, Eigen::Vector3d> &venom_frame)
-        // {
-        //     venom_frame[0]
+        void RotationCamera2Venom(std::vector<Eigen::Vector3d> &venom_axis, int venom_type)
+        {
+            Eigen::Matrix3d R_cm(Eigen::Matrix3d::Zero());
+            // type 0: 0, 1
+            // type 1: 0, 2
+            // type 2: 0, 1, 2
+            // type 3: 1, 2
+            if(venom_type!=3)
+            {
+                R_cm(0,0) = venom_axis[0](0,0);
+                R_cm(1,0) = venom_axis[0](1,0);
+                R_cm(2,0) = venom_axis[0](2,0);
+                if(venom_type==0)
+                {
+                    R_cm(0, 1) = venom_axis[1](0, 0);
+                    R_cm(1, 1) = venom_axis[1](1, 0);
+                    R_cm(2, 1) = venom_axis[1](2, 0);
+                    // cross: axis_0 x axis_1 
+                    Eigen::Vector3d z_x = venom_axis[0].cross(venom_axis[1]);
+                    R_cm(0, 2) = z_x(0, 0);
+                    R_cm(1, 2) = z_x(1, 0);
+                    R_cm(2, 2) = z_x(2, 0);
+                }
+                else if(venom_type==1)
+                {
+                    R_cm(0, 2) = venom_axis[2](0, 0);
+                    R_cm(1, 2) = venom_axis[2](1, 0);
+                    R_cm(2, 2) = venom_axis[2](2, 0);
+                    // cross: axis_0 x axis_1
+                    Eigen::Vector3d z_x = venom_axis[0].cross(venom_axis[2]);
+                    R_cm(0, 1) = z_x(0, 0);
+                    R_cm(1, 1) = z_x(1, 0);
+                    R_cm(2, 1) = z_x(2, 0);
+                }
+                else if(venom_type==2)
+                {
+                    R_cm(0, 1) = venom_axis[1](0, 0);
+                    R_cm(1, 1) = venom_axis[1](1, 0);
+                    R_cm(2, 1) = venom_axis[1](2, 0);
+                    R_cm(0, 2) = venom_axis[2](0, 0);
+                    R_cm(1, 2) = venom_axis[2](1, 0);
+                    R_cm(2, 2) = venom_axis[2](2, 0);
 
-        // }
+                    //TODO: 
+                    // SVD refinement
+                }
+            }else 
+            {
+                R_cm(0, 1) = venom_axis[1](0, 0);
+                R_cm(1, 1) = venom_axis[1](1, 0);
+                R_cm(2, 1) = venom_axis[1](2, 0);
+                R_cm(0, 2) = venom_axis[2](0, 0);
+                R_cm(1, 2) = venom_axis[2](1, 0);
+                R_cm(2, 2) = venom_axis[2](2, 0);
+                // cross: axis_0 x axis_1
+                Eigen::Vector3d z_x = venom_axis[1].cross(venom_axis[2]);
+                R_cm(0, 0) = z_x(0, 0);
+                R_cm(1, 0) = z_x(1, 0);
+                R_cm(2, 0) = z_x(2, 0);
+            }
+            
+
+        }
+
+        
 
 
         
@@ -103,6 +192,7 @@ namespace simulator
         std::vector<std::vector<std::pair<int /* */, Eigen::Matrix<double, 3, 2>>>> obs_line_;
         Trajectory* robot_traject_; 
         std::vector<MapLine*> vec_ptr_mls_;
+        
         
     
     };
